d77fd98ff498ac21bf5f6247033586d98edc2971
Work-PC\Work <gftvfx@gmail.com>
2020-08-19 20:49:29 -0700

    Added pipeline script

---
+++ pipelines/test_pipeline.groovy
@@ -0,0 +1,17 @@
+@Library("shared")
+import gtvfx.jenkins.Helpers
+
+
+pipeline {
+    agent {label "master"}
+    stages {
+        stage('validate Helpers') {
+            steps {
+                script {
+                    def helpers = new Helpers;
+                    print("helpers: " + helpers)
+                }
+            }
+        }
+    }
+}
